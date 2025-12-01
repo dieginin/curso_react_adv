e1b494ca484a8ce1f6efc213815066ac15a20381
dieginin <diegobalestra9@gmail.com>
2025-12-01 15:52:59 -0800

    refactor: simplify state management in CounterBy component

--- src/bases/CounterBy.tsx
+++ src/bases/CounterBy.tsx
@@ -4,22 +4,27 @@ interface Props {
   initialValue?: number
 }
 
+interface CounterState {
+  counter: number
+  clicks: number
+}
+
 export const CounterBy = ({ initialValue = 5 }: Props) => {
-  const [counter, setCounter] = useState({
+  const [{ counter, clicks }, setCounter] = useState<CounterState>({
     counter: initialValue,
     clicks: 0,
   })
 
   const handleAdd = (amount: number) =>
     setCounter({
-      counter: counter.counter + amount,
-      clicks: counter.clicks + 1,
+      counter: counter + amount,
+      clicks: clicks + 1,
     })
 
   return (
     <>
-      <h1>CounterBy: {counter.counter}</h1>
-      <h1>ClickCount: {counter.clicks}</h1>
+      <h1>CounterBy: {counter}</h1>
+      <h1>ClickCount: {clicks}</h1>
       <button onClick={() => handleAdd(1)}>+1</button>
       <button onClick={() => handleAdd(5)}>+5</button>
     </>
