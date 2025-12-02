c5bc5a2bdd661711c413a4e0fd230dfdd54614bd
dieginin <diegobalestra9@gmail.com>
2025-12-01 16:07:05 -0800

    feat: add CounterEffect component to display and manage counter state

---
+++ src/bases/CounterEffect.tsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react"
+
+const MAXIMUM_COUNT = 10
+
+export const CounterEffect = () => {
+  const [counter, setCounter] = useState(4)
+
+  const handleAdd = () => setCounter(Math.min(counter + 1, MAXIMUM_COUNT))
+  // const handleAdd = () => {
+  //   if (counter >= MAXIMUM_COUNT) return
+  //   setCounter(counter + 1)
+  // }
+
+  useEffect(() => {
+    if (counter < MAXIMUM_COUNT) return
+
+    console.log(
+      "%cSe llego al valor máximo",
+      "color: red; background-color:black;"
+    )
+  }, [counter])
+
+  return (
+    <>
+      <h1>CounterEffect: {counter}</h1>
+      <button onClick={handleAdd}>+1</button>
+    </>
+  )
+}
