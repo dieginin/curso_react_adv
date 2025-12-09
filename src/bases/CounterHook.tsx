3b0cc20c1f277897957b442fdae0eb06b2598e5c
dieginin <diegobalestra9@gmail.com>
2025-12-08 22:00:46 -0800

    feat: update useCounter hook to accept maxCount parameter and refactor animation logic

--- src/bases/CounterHook.tsx
+++ src/bases/CounterHook.tsx
@@ -1,12 +1,12 @@
 import { useCounter } from "../hooks/useCounter"
 
 export const CounterHook = () => {
-  const { counter, counterRef, handleAdd } = useCounter()
+  const { counter, elementToAnimate, handleAdd } = useCounter({ maxCount: 15 })
 
   return (
     <>
       <h1>CounterHook:</h1>
-      <h2 ref={counterRef}>{counter}</h2>
+      <h2 ref={elementToAnimate}>{counter}</h2>
       <button onClick={handleAdd}>+1</button>
     </>
   )
--- src/hooks/useCounter.ts
+++ src/hooks/useCounter.ts
@@ -1,31 +1,31 @@
-import { useEffect, useRef, useState } from "react"
+import { useEffect, useLayoutEffect, useRef, useState } from "react"
 
 import gsap from "gsap"
 
-const MAXIMUM_COUNT = 10
-
-export const useCounter = () => {
+export const useCounter = ({ maxCount = 10 }) => {
   const [counter, setCounter] = useState(4)
-  const counterRef = useRef<HTMLHeadingElement>(null)
 
-  const handleAdd = () => setCounter(Math.min(counter + 1, MAXIMUM_COUNT))
+  const elementToAnimate = useRef<any>(null)
+  const tl = useRef(gsap.timeline())
+
+  const handleAdd = () => setCounter(Math.min(counter + 1, maxCount))
+
+  useLayoutEffect(() => {
+    if (!elementToAnimate.current) return
+    tl.current
+      .to(elementToAnimate.current, { y: -10, duration: 0.2, ease: "ease.out" })
+      .to(elementToAnimate.current, { y: 0, duration: 1, ease: "bounce.out" })
+      .pause()
+  }, [])
 
   useEffect(() => {
-    if (counter < MAXIMUM_COUNT) return
-
-    console.log(
-      "%cSe llego al valor máximo",
-      "color: red; background-color:black;"
-    )
-
-    const tl = gsap.timeline()
-    tl.to(counterRef.current, { y: -10, duration: 0.2, ease: "ease.out" })
-    tl.to(counterRef.current, { y: 0, duration: 1, ease: "bounce.out" })
+    // if (counter < maxCount) return
+    tl.current.play(0)
   }, [counter])
 
   return {
     counter,
-    counterRef,
+    elementToAnimate,
     handleAdd,
   }
 }
