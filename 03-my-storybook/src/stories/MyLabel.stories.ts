cd13be93ac36b8454e22b383b2c2d5aed7d9b5f5
dieginin <diegobalestra9@gmail.com>
2025-12-16 09:53:29 -0800

    feat: refactor MyLabel component to accept props and update Storybook stories

--- 03-my-storybook/src/stories/MyLabel.stories.ts
+++ 03-my-storybook/src/stories/MyLabel.stories.ts
@@ -4,10 +4,15 @@ import { MyLabel } from "../components/MyLabel"
 const meta = {
   title: "UI/Labels/MyLabel",
   component: MyLabel,
+  tags: ["autodocs"],
   parameters: { layout: "centered" },
 } satisfies Meta<typeof MyLabel>
 
 export default meta
 type Story = StoryObj<typeof meta>
 
-export const Basic: Story = {}
+export const Basic: Story = {
+  args: {
+    label: "Basic label",
+  },
+}
