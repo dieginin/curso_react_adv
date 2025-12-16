ef363b11d3bcc9dd9b501397b51cbc117bef6573
dieginin <diegobalestra9@gmail.com>
2025-12-16 09:31:30 -0800

    feat: add MyLabel component with styles and Storybook stories

---
+++ 03-my-storybook/src/stories/MyLabel.stories.ts
@@ -0,0 +1,13 @@
+import type { Meta, StoryObj } from "@storybook/react-vite"
+import { MyLabel } from "../components/MyLabel"
+
+const meta = {
+  title: "UI/Labels/MyLabel",
+  component: MyLabel,
+  parameters: { layout: "centered" },
+} satisfies Meta<typeof MyLabel>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Basic: Story = {}
