b2867061c3f7527a00c325c66c9a0208c702f121
dieginin <diegobalestra9@gmail.com>
2025-12-16 19:56:52 -0800

    feat: add PWA support with service worker and Vite plugin
    - Updated package.json to include vite-plugin-pwa for PWA functionality.
    - Created a new service worker (sw.js) to handle caching and offline capabilities.
    - Configured Vite to use the PWA plugin with appropriate settings for service worker registration and caching strategies.

---
+++ 05-PWA/frontend/src/sw.js
@@ -0,0 +1,30 @@
+import { NavigationRoute, registerRoute } from "workbox-routing"
+import {
+  cleanupOutdatedCaches,
+  createHandlerBoundToURL,
+  precacheAndRoute,
+} from "workbox-precaching"
+
+import { clientsClaim } from "workbox-core"
+
+cleanupOutdatedCaches()
+
+// self.__WB_MANIFEST is default injection point
+precacheAndRoute(self.__WB_MANIFEST)
+
+// to allow work offline
+registerRoute(
+  new NavigationRoute(createHandlerBoundToURL("index.html"), {
+    denylist: [/^\/backoffice/],
+  })
+)
+
+self.skipWaiting()
+
+clientsClaim()
+self.addEventListener("install", async (event) => {
+  const cache = await caches.open("cache-1")
+  await cache.addAll([
+    //aca lo que se quiere llevar al cache
+  ])
+})
--- 05-PWA/frontend/vite.config.js
+++ 05-PWA/frontend/vite.config.js
@@ -1,7 +1,22 @@
+import { VitePWA } from "vite-plugin-pwa"
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react-swc"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      devOptions: {
+        enabled: true,
+        type: "module",
+        navigateFallback: "index.html",
+        suppressWarnings: true,
+      },
+      filename: "sw.js",
+      registerType: "autoUpdate",
+      srcDir: "src",
+      strategies: "injectManifest",
+    }),
+  ],
 })
