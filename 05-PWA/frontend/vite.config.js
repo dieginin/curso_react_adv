152c55ddb3de65508c8be5844f9bb3f039540a89
dieginin <diegobalestra9@gmail.com>
2025-12-17 20:30:24 -0800

    feat: configure PWA with Workbox and service worker setup

--- 05-PWA/frontend/vite.config.js
+++ 05-PWA/frontend/vite.config.js
@@ -1,7 +1,20 @@
+import { VitePWA } from "vite-plugin-pwa"
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react-swc"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      devOptions: {
+        enabled: true,
+        type: "module",
+        suppressWarnings: true,
+      },
+      registerType: "autoUpdate",
+      srcDir: "src",
+      strategies: "injectManifest",
+    }),
+  ],
 })
