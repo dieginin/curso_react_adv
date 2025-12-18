a8689bce7f1c0abe75dd20bca23c4c028f7aa617
dieginin <diegobalestra9@gmail.com>
2025-12-17 21:31:21 -0800

    fix: remove obsolete Workbox configuration file

--- 05-PWA/frontend/workbox-config.cjs
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  globDirectory: "dist",
-  globPatterns: ["**/*.{svg,webmanifest,html,css,js}"],
-  swDest: "dist/sw.js",
-  ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
-}
