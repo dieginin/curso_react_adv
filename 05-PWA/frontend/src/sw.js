cc19bebff7ebc8d6457fc8c0689c1882fd755ece
dieginin <diegobalestra9@gmail.com>
2025-12-17 23:50:07 -0800

    refactor: streamline caching strategy and improve route registration for API events

--- 05-PWA/frontend/src/sw.js
+++ 05-PWA/frontend/src/sw.js
@@ -7,27 +7,20 @@ import { registerRoute } from "workbox-routing"
 
 precacheAndRoute([...self.__WB_MANIFEST, { url: "/vite.svg", revision: null }])
 
-registerRoute(
-  new RegExp(
-    "https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css"
-  ),
-  new CacheFirst()
-)
+// registerRoute(
+//   new RegExp("http://localhost:4000/api/auth/renew"),
+//   new NetworkFirst()
+// )
 
-registerRoute(
-  new RegExp(
-    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css"
-  ),
-  new CacheFirst()
-)
+const cacheCacheFirst = [
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css",
+  "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css",
+]
+registerRoute(({ url }) => cacheCacheFirst.includes(url.href), new CacheFirst())
 
+const cacheNetworkFirst = ["/api/auth/renew", "/api/events"]
 registerRoute(
-  new RegExp("http://localhost:4000/api/auth/renew"),
-  new NetworkFirst()
-)
-
-registerRoute(
-  new RegExp("http://localhost:4000/api/events"),
+  ({ url }) => cacheNetworkFirst.includes(url.pathname),
   new NetworkFirst()
 )
 
@@ -45,7 +38,7 @@ registerRoute(
 )
 
 registerRoute(
-  new RegExp("http://localhost:4000/api/events/"),
+  new RegExp("http://localhost:4000/api/events/*"),
   new NetworkOnly({
     plugins: [bgSyncPlugin],
   }),
@@ -53,7 +46,7 @@ registerRoute(
 )
 
 registerRoute(
-  new RegExp("http://localhost:4000/api/events/"),
+  new RegExp("http://localhost:4000/api/events/*"),
   new NetworkOnly({
     plugins: [bgSyncPlugin],
   }),
