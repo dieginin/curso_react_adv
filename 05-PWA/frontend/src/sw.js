0cc0f21d6ce666d6e96ac385aa905b2b6847b161
dieginin <diegobalestra9@gmail.com>
2025-12-17 21:34:27 -0800

    feat: enhance service worker with additional caching strategies for external resources

--- 05-PWA/frontend/src/sw.js
+++ 05-PWA/frontend/src/sw.js
@@ -1,3 +1,30 @@
-import { precacheAndRoute } from "workbox-precaching"
+import { CacheFirst, NetworkFirst } from "workbox-strategies"
 
-precacheAndRoute(self.__WB_MANIFEST)
+import { precacheAndRoute } from "workbox-precaching"
+import { registerRoute } from "workbox-routing"
+
+precacheAndRoute([...self.__WB_MANIFEST, { url: "/vite.svg" }])
+
+registerRoute(
+  new RegExp(
+    "https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css"
+  ),
+  new CacheFirst()
+)
+
+registerRoute(
+  new RegExp(
+    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css"
+  ),
+  new CacheFirst()
+)
+
+registerRoute(
+  new RegExp("http://localhost:4000/api/auth/renew"),
+  new NetworkFirst()
+)
+
+registerRoute(
+  new RegExp("http://localhost:4000/api/events"),
+  new NetworkFirst()
+)
