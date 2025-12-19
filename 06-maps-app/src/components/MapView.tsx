2424abe87eab01428195a6823bbd8c897b6cbcd6
dieginin <diegobalestra9@gmail.com>
2025-12-18 23:08:23 -0800

    feat: integrate Mapbox GL JS for map rendering in MapView component

--- 06-maps-app/src/components/MapView.tsx
+++ 06-maps-app/src/components/MapView.tsx
@@ -1,11 +1,36 @@
+import { useContext, useLayoutEffect, useRef } from "react"
+
 import { Loading } from "./"
+import { Map } from "mapbox-gl"
 import { PlacesContext } from "../contexts"
-import { useContext } from "react"
 
 export const MapView = () => {
   const { isLoading, userLocation } = useContext(PlacesContext)
+  const mapDiv = useRef<HTMLDivElement>(null)
+
+  useLayoutEffect(() => {
+    if (!isLoading) {
+      new Map({
+        container: mapDiv.current!,
+        style: "mapbox://styles/mapbox/streets-v12",
+        center: userLocation,
+        zoom: 14,
+      })
+    }
+  }, [isLoading, userLocation])
 
   if (isLoading) return <Loading />
 
-  return <div>{userLocation?.join(", ")}</div>
+  return (
+    <div
+      ref={mapDiv}
+      style={{
+        height: "100vh",
+        left: 0,
+        position: "fixed",
+        top: 0,
+        width: "100vw",
+      }}
+    />
+  )
 }
--- 06-maps-app/src/main.tsx
+++ 06-maps-app/src/main.tsx
@@ -1,11 +1,15 @@
 import { MapsApp } from "./MapsApp"
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
+import mapboxgl from "mapbox-gl"
 
 if (!navigator.geolocation) {
   alert("Tu navegador no tiene acceso de GeoLocation")
   throw new Error("Tu navegador no tiene acceso de GeoLocation")
 }
+
+mapboxgl.accessToken =
+  "pk.eyJ1Ijoid2Fja3MiLCJhIjoiY21qY2h1Mm54MHBsMjNjb3d6YmlrbXdlNSJ9.3yF2dxjcf2gnUbllO0NIdw"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
