228e92c234afa9fdfa5ba9d56981b01b6f7a4d29
dieginin <diegobalestra9@gmail.com>
2025-12-18 23:12:57 -0800

    refactor: adjust PlacesContextProps interface export in PlacesContext

--- 06-maps-app/src/contexts/index.ts
+++ 06-maps-app/src/contexts/index.ts
@@ -1,3 +1,3 @@
+export { PlacesContext } from "./places/PlacesContext"
 export { placesReducer } from "./places/places.reducer"
-export { type PlacesContextProps, PlacesContext } from "./places/PlacesContext"
 export { type PlacesState, PlacesProvider } from "./places/PlacesProvider"
