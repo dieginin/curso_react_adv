d1ed7246790c3ad57ded5e264cd1b78c8b6c80d7
dieginin <diegobalestra9@gmail.com>
2025-12-19 22:45:08 -0800

    feat: add route visualization with polyline and bounds adjustment in MapProvider

--- 06-maps-app/src/contexts/map/MapProvider.tsx
+++ 06-maps-app/src/contexts/map/MapProvider.tsx
@@ -1,4 +1,10 @@
-import { Marker, Popup, type Map } from "mapbox-gl"
+import {
+  LngLatBounds,
+  Marker,
+  Popup,
+  type Map,
+  type SourceSpecification,
+} from "mapbox-gl"
 import { MapContext, mapReducer, PlacesContext } from ".."
 import {
   useContext,
@@ -69,14 +75,59 @@ export const MapProvider = ({ children }: PropsWithChildren) => {
     )
 
     const { distance, duration, geometry } = resp.data.routes[0]
+    const { coordinates: coords } = geometry
 
     let kms = distance / 1000
     kms = Math.round(kms * 100)
     kms /= 100
-
     const mins = Math.floor(duration / 60)
-
     console.log({ kms, mins })
+
+    // * Zoom
+    const bounds = new LngLatBounds(start, start)
+    for (const coord of coords) {
+      const newCoord: [number, number] = [coord[0], coord[1]]
+      bounds.extend(newCoord)
+    }
+    state.map?.fitBounds(bounds, { padding: 150 })
+
+    // * Polyline
+    const sourceData: SourceSpecification = {
+      type: "geojson",
+      data: {
+        type: "FeatureCollection",
+        features: [
+          {
+            type: "Feature",
+            properties: {},
+            geometry: {
+              type: "LineString",
+              coordinates: coords,
+            },
+          },
+        ],
+      },
+    }
+
+    if (state.map?.getLayer("RouteString")) {
+      state.map.removeLayer("RouteString")
+      state.map.removeSource("RouteString")
+    }
+
+    state.map?.addSource("RouteString", sourceData)
+    state.map?.addLayer({
+      id: "RouteString",
+      type: "line",
+      source: "RouteString",
+      layout: {
+        "line-cap": "round",
+        "line-join": "round",
+      },
+      paint: {
+        "line-color": "black",
+        "line-width": 3,
+      },
+    })
   }
 
   return (
